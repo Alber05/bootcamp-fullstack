ed98044f19f0fb02200a6f0e3bdc4d5b1b161f6f
Alberto Sánchez <albersanchez511@hotmail.com>
2023-07-12 14:10:38 +0200

    Añadido ejercicio courseInfo de la parte 1

---
+++ part1/courseinfo/src/components/Content.js
@@ -0,0 +1,11 @@
+import { Part } from "./Part";
+
+export const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
